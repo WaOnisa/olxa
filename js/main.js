6cb335d6a3aad452982f86d316e460d50f53223e
Anastasia Kornoukhova <vaonisa@gmail.com>
2024-10-21 19:09:34 +0500

    add main slider

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 const swiper = new Swiper('.swiper', {
-	direction: 'vertical',
+	// Optional parameters
+	direction: 'horizontal',
 	loop: true,
+
+	// If we need pagination
 	pagination: {
 		el: '.swiper-pagination',
 	},
+
+	// Navigation arrows
 	navigation: {
 		nextEl: '.swiper-button-next',
 		prevEl: '.swiper-button-prev',
