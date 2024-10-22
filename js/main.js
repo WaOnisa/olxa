32da829e4ed47efad3ea1ecb0691a5c03710ef12
Anastasia Kornoukhova <vaonisa@gmail.com>
2024-10-22 23:52:30 +0500

    add services

--- js/main.js
+++ js/main.js
@@ -1,16 +1,29 @@
-const swiper = new Swiper('.swiper', {
-	// Optional parameters
+const swiper1 = new Swiper('.swiper1', {
+
 	direction: 'horizontal',
 	loop: true,
 
-	// If we need pagination
 	pagination: {
-		el: '.swiper-pagination',
+		el: '.swiper-pagination1',
 	},
 
-	// Navigation arrows
 	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev',
+		nextEl: '.swiper-button-next1',
+		prevEl: '.swiper-button-prev1',
+	},
+})
+
+const swiper2 = new Swiper('.swiper2', {
+
+	direction: 'horizontal',
+	loop: true,
+
+	pagination: {
+		el: '.swiper-pagination2',
+	},
+
+	navigation: {
+		nextEl: '.swiper-button-next2',
+		prevEl: '.swiper-button-prev2',
 	},
 })
